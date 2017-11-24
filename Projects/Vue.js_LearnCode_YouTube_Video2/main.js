b065f21505de6cb0656877f5b3d8ce332cdfeff7
babbott <babbott@esemos.de>
2017-11-24 13:53:17 +0100

    added Vue projects

---
+++ Projects/Vue.js_LearnCode_YouTube_Video2/main.js
@@ -0,0 +1,23 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        bobby: {
+            name: 'Bobby Boone',
+            age: 25,
+        },
+        john: {
+            name: 'John Bobby',
+            age: 35,
+        }        
+    },
+    template: `
+        <div>
+        <h2>Name: {{bobby.name}}</h2>
+        <h2>Age: {{bobby.age}}</h2>        
+
+        <h2>Name: {{john.name}}</h2>
+        <h2>Age: {{john.age}}</h2>        
+
+        </div>
+    `
+})
